120cade1a8a41e3e4acef015b78820cd2e96a1b6
Nav <nk95@hotmail.co.uk>
2018-10-30 11:32:47 +0000

    Source code

---
+++ src/main/java/com/qa/AccountApp/accountApp/AccountSystem.java
@@ -0,0 +1,27 @@
+package com.qa.AccountApp.accountApp;
+
+import java.util.HashMap;
+
+public class AccountSystem {
+	
+	HashMap<Integer, Account> bankAccounts = new HashMap<Integer, Account>();
+	static int counter = 0;
+	
+	public void addAccount(int counter, String firstName, String secondName, String accountNumber) {
+		
+		bankAccounts.put(counter, new Account(firstName, secondName, accountNumber));
+	}
+	
+	public void getAccount(int id) 
+	{
+		System.out.println(bankAccounts.get(id));	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
--- src/main/java/com/qa/AccountApp/accountApp/App.java
+++ src/main/java/com/qa/AccountApp/accountApp/App.java
@@ -1,13 +1,19 @@
 package com.qa.AccountApp.accountApp;
 
-/**
- * Hello world!
- *
- */
+import java.util.HashMap;
+
+
 public class App 
 {
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    AccountSystem accountSystem = new AccountSystem();
+    
+    accountSystem.addAccount(0, "John", "Bishop", "19202711");
+    accountSystem.addAccount(1, "Perry", "Bishop", "19122711");	
+    
+  	accountSystem.getAccount(0);
+    	
     }
 }
