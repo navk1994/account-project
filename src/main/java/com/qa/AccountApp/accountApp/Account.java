51897f353f6ef328a8a3fc865a002256ee2df0c7
Nav <nk95@hotmail.co.uk>
2018-11-01 09:19:20 +0000

    Code working with H2

--- src/main/java/com/qa/AccountApp/accountApp/Account.java
+++ src/main/java/com/qa/AccountApp/accountApp/Account.java
@@ -1,9 +1,23 @@
 package com.qa.AccountApp.accountApp;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Account {
 	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@Column(length = 100)
 	private String firstName;
+	@Column(length = 50)
 	private String lastName;
+	@Column(length = 4)
 	private String accountNumber;
 	
 	public Account(String firstName, String lastName, String accountNumber)
